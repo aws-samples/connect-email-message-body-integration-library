7288af7178ccedc50a96071fb1c0d516b1e48d4b
Evangeline Ferrell <38958921+ejferr@users.noreply.github.com>
2024-11-13 22:01:31 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,17 +1,107 @@
-## My Project
+## Prerequisites
 
-TODO: Fill this README out!
+You must have the following prerequisites:
 
-Be sure to:
+- An [AWS account](https://signin.aws.amazon.com/signin)
+- The [AWS CLI v2](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html)
+- Python 3.11 or later
+- AWS CDK
+- An Amazon Connect Instance with Email for Amazon Connect enabled
+- If you don’t already have the AWS CLI on your local machine, refer to Installing or updating the latest version of the AWS 
+CLI and Configuring the AWS CLI.
 
-* Change the title in this README
-* Edit your repository description on GitHub
+Install the AWS CDK Toolkit globally using the following node package manager command:
+- npm install -g aws-cdk-lib@latest
 
-## Security
+### Guidance for completing .env file
+You must have the following pieces of information in order to fill in the required values:
+- The bucket name associated to your Amazon Connect Instance
+- The instance name associated to your Amazon Connect Instance
+- The instance arn associated to your Amazon Connect Instance
+    - go to the Amazon Connect in the AWS console
+    - select the instance alias you want to use for this solution
+    - copy and paste the arn
+- The hours of service arn associated to your Amazon Connect Instance found in the hours of operation in your instance
+    - select the hours of opperation you want to associate to the queues
+    - select Show additional hours of operation information and copy and paste the arn
 
-See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
+#### Setup a virtual environment
 
-## License
+This project is set up like a standard Python project. Create a Python virtual environment using the following code:
 
-This library is licensed under the MIT-0 License. See the LICENSE file.
+```
+python3 -m venv .venv
+```
 
+Use the following command to activate the virtual environment for a MAC os:
+
+```
+source .venv/bin/activate
+```
+
+If you’re on a Windows platform, activate the virtual environment as follows:
+
+```
+.venv\Scripts\activate.bat
+```
+
+```
+python3 -m pip install --upgrade pip
+```
+
+Install the required dependencies:
+
+```
+pip install -r requirements.txt
+```
+
+```
+update the values in the .env file
+```
+
+Before you deploy any AWS CDK application, you need to bootstrap a space in your account and the Region you’re deploying into. To bootstrap in your default Region, issue the following command:
+
+```
+cdk bootstrap
+```
+
+If you want to deploy into a specific account and Region, issue the following command:
+
+```
+cdk bootstrap aws://ACCOUNT-NUMBER/REGION
+```
+* `cdk synth`    - Emits the synthesized AWS CloudFormation template
+ * `cdk deploy`    - Deploys this stack to your default AWS account and Region
+ #### Go to the AWS Console
+ - navigate to Amazon Connect using the search bar at the top left
+ - select your instance alias
+ - navigate to flows
+ - click the dropdown to select the EmailAutomation-LambdaFunction lambda
+ - attach the lambda
+ - Once you have attached your lambda in the search box at the top of the page type Bedrock
+ - Select Amazon Bedrock
+ - Click the hamburger menu on the left hand side
+ - Scroll to the bottom on the left hand side of the page to Bedrock configurations
+ - Click Model access
+ - Under Anthropic make sure that Claude Haiku shows the Access status of "Access granted"
+    - If not click the orange button at the top of the page that says "Modify model access"
+    - Click the box next to Claude 3 Haiku
+    - Scroll to the bottom of the page and click next
+    - Review and submit the access request "You should see a status of "Access granted"
+    
+ #### Go to your Amazon Connect Instance
+ - go to Contact Flows
+ - find the contact flow EmailRoutingIntelligence
+ - go to the Lambda block
+ - select the lambda function from the drop down
+ - you should see 4 set queue blocks and will likely have to reselect the queue for each
+ - once complete click Publish and the flow will save and publish for testing
+ - assign the queues to the routing profile(s) of your choice and ensure that the email channel is checked
+ - attach the flow to the email address of your choice and you are ready to test.
+ 
+ #### Clean-up
+ - In the Amazon Connect Instance:
+   - Remove the Contact Flow association to email address(s)
+   - Remove the Queue associations to routing profile(s)
+ - Once the above has been done: 
+   - In the CLI execute the command cdk destroy --all
---
+++ app.py
@@ -0,0 +1,160 @@
+from aws_cdk import (
+    App,
+    Stack,
+    aws_lambda as lambda_,
+    aws_connect as connect,
+    aws_iam as iam,
+    CfnOutput,
+    Duration,
+)
+from constructs import Construct
+import os
+from dotenv import load_dotenv
+import json
+import uuid
+import logging
+import subprocess
+import shutil
+import tempfile
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class EmailAutomation(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # Validate environment variables
+        required_env_vars = ['CONNECT_INSTANCE_ARN', 'HOURS_OF_OPERATION_ARN', 'CONNECT_BUCKET', 'INSTANCE_NAME']
+        for var in required_env_vars:
+            if not os.environ.get(var):
+                raise ValueError(f"Environment variable {var} is not set")
+
+        connect_instance_arn = os.environ['CONNECT_INSTANCE_ARN']
+        hours_of_operation_arn = os.environ['HOURS_OF_OPERATION_ARN']
+        environment = {
+           "connectBucket": os.environ['CONNECT_BUCKET'],
+           "instName": os.environ['INSTANCE_NAME']
+        }
+
+        # Generate a unique ID for the contact flow
+        contact_flow_id = str(uuid.uuid4())
+
+        # Build the Lambda layer
+        layer_asset_path = self.build_layer()
+
+        # Create a Lambda Layer
+        lambda_layer = lambda_.LayerVersion(
+            self, "LambdaLayer",
+            code=lambda_.Code.from_asset(layer_asset_path),
+            compatible_runtimes=[lambda_.Runtime.PYTHON_3_11],
+            description="Lambda Layer for boto3 and other dependencies"
+        )
+
+        # Create a Python Lambda function
+        lambda_fn = lambda_.Function(
+            self, "LambdaFunction",
+            code=lambda_.Code.from_asset("./lambda"),
+            handler="lambda_function.lambda_handler",
+            runtime=lambda_.Runtime.PYTHON_3_11,
+            layers=[lambda_layer],
+            environment=environment,
+            timeout=Duration.seconds(30),
+            memory_size=256,
+            tracing=lambda_.Tracing.ACTIVE  # Enable X-Ray tracing
+        )
+
+        # Add IAM permissions for Amazon Connect API access (scoped down)
+        lambda_fn.add_to_role_policy(iam.PolicyStatement(
+            actions=[
+                "connect:*"
+            ],
+            resources=[f"{connect_instance_arn}/*"]
+        ))
+
+        # Add IAM permissions for Amazon Bedrock model access (Claude Haiku)
+        lambda_fn.add_to_role_policy(iam.PolicyStatement(
+            actions=[
+                "bedrock:InvokeModel"
+            ],
+            resources=[
+                f"arn:aws:bedrock:{self.region}::foundation-model/anthropic.claude-3-haiku-20240307-v1:0"
+            ]
+        ))
+
+        # Create Queues
+        queue_names = ["HomeEquity", "CarLoan", "HomeLoan", "Unknown"]
+        queues = {}
+        for queue_name in queue_names:
+            queue = connect.CfnQueue(
+                self, f"Queue{queue_name}",
+                instance_arn=connect_instance_arn,
+                name=queue_name,
+                description=f"{queue_name} Specialists",
+                hours_of_operation_arn=hours_of_operation_arn,
+                tags=[{
+                    "key": "Environment",
+                    "value": "Production"
+                    }, {
+                    "key": "SecurityClassification",
+                    "value": "Confidential"
+                }]
+            )
+            queues[queue_name] = queue
+
+        # Create Contact Flow
+        try:
+            with open("./email_assessor/Intent_Routing_Email_Flow.content.json", "r") as file:
+                content = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+            logger.error(f"Error reading contact flow file: {str(e)}")
+            raise
+
+        # Update queue references in the content
+        for action in content['Actions']:
+            if action['Type'] == 'UpdateContactTargetQueue':
+                queue_name = action['Parameters']['QueueId'].split('/')[-1]
+                if queue_name in queues:
+                    action['Parameters']['QueueId'] = queues[queue_name].attr_queue_arn
+
+        cfn_contact_flow = connect.CfnContactFlow(self, "EmailRoutingContactFlow",
+            content=json.dumps(content),
+            instance_arn=connect_instance_arn,
+            name="EmailRoutingIntelligence",
+            type="CONTACT_FLOW",
+            description="Inbound contact flow for email routing",
+            state="ACTIVE",
+        )
+
+        # Outputs
+        CfnOutput(self, "LambdaFunctionARN", value=lambda_fn.function_arn)
+        CfnOutput(self, "LambdaLayerARN", value=lambda_layer.layer_version_arn)
+        CfnOutput(self, "ContactFlowARN", value=cfn_contact_flow.attr_contact_flow_arn)
+        for queue_name, queue in queues.items():
+            CfnOutput(self, f"Queue{queue_name}ARN", value=queue.attr_queue_arn)
+
+    def build_layer(self):
+        # Create a temporary directory that will persist
+        layer_build_dir = tempfile.mkdtemp(prefix="lambda_layer_")
+        
+        # Create a directory for the layer
+        layer_dir = os.path.join(layer_build_dir, 'python')
+        os.makedirs(layer_dir)
+        
+        # Install the latest boto3 and other dependencies
+        subprocess.run(["pip", "install", "boto3", "-t", layer_dir], check=True)
+        
+        # Create a zip file of the layer contents
+        layer_zip = os.path.join(layer_build_dir, 'layer.zip')
+        shutil.make_archive(os.path.join(layer_build_dir, 'layer'), 'zip', layer_dir)
+        
+        # Return the path to the zip file
+        return layer_zip
+
+app = App()
+EmailAutomation(app, "EmailAutomation")
+app.synth()
